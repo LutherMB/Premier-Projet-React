2da7a110a6685141e7715bd415cd04b5c6dea469
LutherMB <mbasse.luther.king@gmail.com>
2022-08-25 05:41:48 +0200

    code source

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey ! 👋
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="http://localhost:3000/"
+          // target="_blank"
           rel="noopener noreferrer"
         >
           Learn React

68644c726ffc57afe1ebea1c922bf57d5263b2c2
LutherMB <mbasse.luther.king@gmail.com>
2022-08-30 22:48:37 +0200

    back add deleteUser function

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -36,3 +36,15 @@ exports.updateUser = async (req, res) => {
     return res.status(500).json({ message: err });
   }
 };
+
+exports.deleteUser = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID invalide : " + req.params.id);
+
+  try {
+    await UserModel.remove({ _id: req.params.id }).exec();
+    res.status(200).json({ message: "Successfully deleted" });
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -7,5 +7,6 @@ router.post("/register", authController.signUp);
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
